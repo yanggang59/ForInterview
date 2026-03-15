0f732d3718d8c287803da0b071ae5958807163f2
yanggang <1449381582@qq.com>
2026-03-15 16:35:30 +0800

    add more
    Signed-off-by: yanggang <1449381582@qq.com>

--- Test/main.c
+++ Test/main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <sys/cdefs.h>
+
+void __attribute__((weak)) test1();
+
+void __attribute__((weak)) test()
+{
+    printf("I am a weak function\r\n");
+}
 
 int main()
 {
     printf("sizeof(int) = %ld\r\n", sizeof(int));
+    printf("sizeof(short int) = %ld\r\n", sizeof(short int));
     printf("sizeof(long) = %ld\r\n", sizeof(long));
+    test();
+    if (test1)
+        test1();
 }
